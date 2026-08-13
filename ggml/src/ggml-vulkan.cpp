bd7cf7b5149f77655779f45c1a7de0586173b81b
Yap Sok Ann <sokann@gmail.com>
2026-08-13 15:51:40 +0700

    ggml-vulkan: Q8_1 + packed-table decode for IQ3_K
    Add the IQ3_K base-IQK Q8_1 mul_mat_vec shader (shared packed iq3nl_values
    table, 16-threads-per-block). IQ2_K models store attn_output as IQ3_K, so this
    lifts the IQ2_K 32B model from ~23.3 to ~26.8 tok/s.

--- ggml/src/ggml-vulkan.cpp
+++ ggml/src/ggml-vulkan.cpp
@@ -2923,6 +2923,7 @@ static void ggml_vk_load_shaders(vk_device& device) {
         CREATE_MMV_Q8_1(GGML_TYPE_IQ3_KT, iq3_kt)
         CREATE_MMV_Q8_1(GGML_TYPE_IQ4_KT, iq4_kt)
         CREATE_MMV_Q8_1(GGML_TYPE_IQ2_K, iq2_k)
+        CREATE_MMV_Q8_1(GGML_TYPE_IQ3_K, iq3_k)
         CREATE_MMV_Q8_1(GGML_TYPE_IQ4_KSS, iq4_kss)
         CREATE_MMV_Q8_1(GGML_TYPE_IQ4_KS, iq4_ks)
         CREATE_MMV_Q8_1(GGML_TYPE_IQ4_K, iq4_k)
@@ -4394,6 +4395,7 @@ static bool ggml_vk_is_iqk_q8_1_type(ggml_type type) {
         case GGML_TYPE_IQ3_KT:
         case GGML_TYPE_IQ4_KT:
         case GGML_TYPE_IQ2_K:
+        case GGML_TYPE_IQ3_K:
         case GGML_TYPE_IQ4_KSS:
         case GGML_TYPE_IQ4_KS:
         case GGML_TYPE_IQ4_K:
@@ -4617,6 +4619,7 @@ static vk_pipeline ggml_vk_get_dequantize_mul_mat_vec_q8_1(ggml_backend_vk_conte
         case GGML_TYPE_IQ3_KT:
         case GGML_TYPE_IQ4_KT:
         case GGML_TYPE_IQ2_K:
+        case GGML_TYPE_IQ3_K:
         case GGML_TYPE_IQ4_KSS:
         case GGML_TYPE_IQ4_KS:
         case GGML_TYPE_IQ4_K:
